5c4117d5e591e87b212a4fec9e9b8ae9aef77e05
Stephanie Adams <adamsstephaniem@gmail.com>
2016-12-02 14:58:05 -0500

    initial commit

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero'
+
+
+@Component({
+  selector: 'my-hero-detail',
+  template: `
+    <div *ngIf="hero">
+    <h2>{{hero.name}} details!</h2>
+      <div>
+        <label>Name: </label>
+        <input  [ngModel]="hero.name" />
+      </div>
+      <div>
+        <label>ID: </label>
+        {{hero.id}}
+      </div>
+    </div>`
+})
+
+export class HeroDetailComponent {
+  @Input()
+  hero : Hero;
+}
