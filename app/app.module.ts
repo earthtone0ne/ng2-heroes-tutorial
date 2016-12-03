eb38330dfaf535e6612f0e7fe82e2fd97ec4e3a5
Stephanie Adams <adamsstephaniem@gmail.com>
2016-12-03 12:43:32 -0500

    begin to add routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
-import { HeroDetailComponent } from './hero-detail.component'
+import { RouterModule } from '@angular/router'
 
+import { HeroDetailComponent } from './hero-detail.component'
+import { HeroesComponent } from './heroes .component'
 import { AppComponent }  from './app.component';
+import { HeroService } from './hero-service'
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: 'HeroesComponent'
+      }
+    ])
+  ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  providers: [ HeroService ]
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
